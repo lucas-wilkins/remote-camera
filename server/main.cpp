5d1dc932e1678ca942b25e4e9f1f33a773dec283
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2026-08-19 14:03:09 +0100

    Debug

--- server/main.cpp
+++ server/main.cpp
@@ -196,6 +196,7 @@ public:
             controlServer = new ControlServer(control_port);
             dataServer = new DataServer(data_port);
 
+            /*
             // callbacks
             controlServer->setCaptureCallback([this]()
             {
@@ -216,6 +217,7 @@ public:
             {
                 return statusCallback();
             });
+            */
 
             // Make data errors get sent to control server
             dataServer->setErrorMessageCallback(controlServer->getErrorMessageCallback());
